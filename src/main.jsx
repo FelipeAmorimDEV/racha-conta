681eab1780d81020ab808441af5fdb60c9a54adb
Felipe Amorim <felipedev@DESKTOP-2VPR9IB>
2024-03-23 14:12:28 -0300

    🔨 fix importação do app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { App } from './App.jsx'
 import './index.css'
 
 const element = document.querySelector('[data-js="root"]')
