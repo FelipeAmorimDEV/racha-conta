c9d8f9e7209992a9d03a3088596cea7d696d6229
Felipe Amorim <felipedev@DESKTOP-2VPR9IB>
2024-03-23 14:13:43 -0300

    ✔  add logica cor & msg status / add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,51 @@
+const initialFriends = [
+  {
+    id: crypto.randomUUID(),
+    name: 'Alessandra',
+    imgUrl: '/friends/alessandra-48.jpg',
+    balance: 100,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Antonio',
+    imgUrl: '/friends/antonio-48.jpg',
+    balance: -50,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Renata',
+    imgUrl: 'friends/renata-48.jpg',
+    balance: 0,
+  },
+]
+
+const getStatusMsg = (balance) =>
+  balance < 0
+    ? { color: 'red-debit', msg: `Você deve ${Math.abs(balance)} reais` }
+    : balance > 0
+      ? { color: 'green-credit', msg: `Te deve ${balance} reais` }
+      : { color: 'white-neutral', msg: 'Estão quites' }
+
 function App() {
-  return <h1>Vite Boilerplate</h1>
+  return (
+    <main className="app">
+      <aside className="sidebar">
+        <ul>
+          {initialFriends.map((friend) => {
+            const { color, msg } = getStatusMsg(friend.balance)
+            return (
+              <li key={friend.id}>
+                <img src={friend.imgUrl} alt={`Foto de ${friend.name}`} />
+                <h3>{friend.name}</h3>
+                <p className={color}>{msg}</p>
+                <button className="button">Selecionar</button>
+              </li>
+            )
+          })}
+        </ul>
+      </aside>
+    </main>
+  )
 }
 
-export default App
+export { App }
