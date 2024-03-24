9809c10b65e192af02e7fc6ddf3ffa05341f8483
Felipe Amorim <felipedev@DESKTOP-2VPR9IB>
2024-03-24 00:34:28 -0300

    ✔  logica add friend / splitbill abstraida para o hook usesplitbill

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,20 @@
-import { useState } from 'react'
-
 import { Header } from './components/header'
 import { Friend } from './components/friend'
 import { AddFriendModal } from './components/add-friend-modal'
 import { SplitBillModal } from './components/split-bill-modal'
 
-const initialFriends = [
-  {
-    id: crypto.randomUUID(),
-    name: 'Alessandra',
-    imgUrl: '/friends/alessandra-48.jpg',
-    balance: 100,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: 'Antonio',
-    imgUrl: '/friends/antonio-48.jpg',
-    balance: -50,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: 'Renata',
-    imgUrl: 'friends/renata-48.jpg',
-    balance: 0,
-  },
-]
+import { useSplitBill } from './hooks/use-split-bill'
 
 function App() {
-  const [selectedFriend, setSelectedFriend] = useState(null)
-  const [friends, setFriends] = useState(initialFriends)
-  const [addFriendModal, setAddFriendModal] = useState(false)
-
-  const splitBill = (transactionValue) => {
-    setFriends((f) =>
-      f.map((friend) =>
-        friend.id === selectedFriend.id
-          ? { ...friend, balance: friend.balance + transactionValue }
-          : friend,
-      ),
-    )
-    setSelectedFriend(null)
-  }
-
-  const addNewFriend = (friendInputs) => {
-    const { name, imgUrl } = friendInputs
-
-    setFriends((f) => [
-      ...f,
-      { id: crypto.randomUUID(), balance: 0, name, imgUrl },
-    ])
-
-    setAddFriendModal(false)
-  }
-
-  const handleOpenFriendModal = () => {
-    setAddFriendModal((adm) => !adm)
-    setSelectedFriend(null)
-  }
-
-  const handleOpenSplitBillModal = (friend) => {
-    setSelectedFriend((sf) => (sf?.id === friend.id ? null : friend))
-    setAddFriendModal(false)
-  }
+  const {
+    friends,
+    selectedFriend,
+    addFriendModal,
+    addNewFriend,
+    splitBill,
+    openFriendModal,
+    openSplitBillModal,
+  } = useSplitBill()
 
   return (
     <>
@@ -73,7 +26,7 @@ function App() {
               <Friend
                 key={friend.id}
                 friend={friend}
-                onOpenSplitBillModal={handleOpenSplitBillModal}
+                onOpenSplitBillModal={openSplitBillModal}
                 selectedFriend={selectedFriend}
               />
             ))}
@@ -81,7 +34,7 @@ function App() {
           {addFriendModal && <AddFriendModal onAddNewFriend={addNewFriend} />}
           <button
             className={`button ${addFriendModal ? 'button-close' : ''}`}
-            onClick={handleOpenFriendModal}
+            onClick={openFriendModal}
           >
             {addFriendModal ? 'Fechar' : 'Adicionar amigo(a)'}
           </button>
